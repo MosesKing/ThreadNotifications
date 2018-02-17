cc3a8ca24a9208fac64a50a97426e9c8457b8412
MosesKing <mkc42@mail.missouri.edu>
2018-02-16 23:42:39 -0600

    Adding Files to Respository

---
+++ src/taskers/Task3.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taskers;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import javafx.application.Platform;
+
+/**
+ *
+ * @author dalemusser
+ * 
+ * This example uses PropertyChangeSupport to implement
+ * property change listeners.
+ * 
+ */
+public class Task3 extends Thread {
+    
+    private int maxValue, notifyEvery;
+    boolean exit = false;
+    private StateOfTheThread currentStateOfTheThread;
+    
+    private Notifiable notificationTarget;
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    
+    public Task3(int maxValue, int notifyEvery)  {
+        this.maxValue = maxValue;
+        this.notifyEvery = notifyEvery;
+    }
+    
+    @Override
+    public void run() {
+        currentStateOfTheThread = StateOfTheThread.RUNNING;
+        doNotify("Task3 start.");
+        for (int i = 0; i < maxValue; i++) {
+            
+            if (i % notifyEvery == 0) {
+                doNotify("It happened in Task3: " + i);
+            }
+            
+            if (exit) {
+                return;
+            }
+        }
+        doNotify("Task3 done.");
+    }
+    
+    public void end() {
+        currentStateOfTheThread = StateOfTheThread.INTERCEPTED;
+        exit = true;
+    }
+    
+    // the following two methods allow property change listeners to be added
+    // and removed
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+         pcs.addPropertyChangeListener(listener);
+     }
+
+     public void removePropertyChangeListener(PropertyChangeListener listener) {
+         pcs.removePropertyChangeListener(listener);
+     }
+    
+    public void setNotificationTarget(Notifiable notificationTarget) {
+        this.notificationTarget = notificationTarget;
+    }
+    
+    private void doNotify(String message) {
+        // this provides the notification through a method on a passed in object (notificationTarget)
+        if (notificationTarget != null) {
+            Platform.runLater(() -> {
+                    pcs.firePropertyChange("message", "", message);
+            });
+        }
+    }
+    
+    public StateOfTheThread getCurrentState() {
+        return currentStateOfTheThread;
+    }
+    public void setCurrentState(StateOfTheThread state){
+        this.currentStateOfTheThread = state;
+    }
+}
